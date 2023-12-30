23d32a98b2ce58ab52b9d05311704782ce6afb4f
yorck25 <100796096+yorck25@users.noreply.github.com>
2023-12-31 00:12:18 +0100

    * adds inventory header and style file

---
+++ src/components/inventory/inventoryHeader/index.tsx
@@ -0,0 +1,8 @@
+import style from './style.module.scss';
+export const InventoryHeader = () => {
+    return (
+        <div className={style.inventory_header}>
+            <button>Add item</button>
+        </div>
+    )
+}
