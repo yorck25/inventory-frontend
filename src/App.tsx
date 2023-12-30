74a81355e65512935202f24c97b4c924b955005f
yorck25 <100796096+yorck25@users.noreply.github.com>
2023-12-30 20:13:40 +0100

    * adds router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from 'react-router-dom';
 
 function App() {
+
+  const router = createBrowserRouter(
+      createRoutesFromElements(
+          <Route path='/' element={<App/>}>
+            {/* eslint-disable-next-line react/jsx-pascal-case */}
+            <Route index element={<App/>}></Route>
+          </Route>
+      )
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <RouterProvider router={router}/>
+      </div>
   );
 }
-
-export default App;
