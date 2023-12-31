0d2cf671262d6029806029cca5767e6398debe93
yorck25 <100796096+yorck25@users.noreply.github.com>
2023-12-31 03:53:51 +0100

    * adds test dummy data, * adds list component

--- src/pages/inventoryPage/index.tsx
+++ src/pages/inventoryPage/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { InventoryHeader } from '../../components/inventory/inventoryHeader'
+import { InventoryItemList } from '../../components/inventory/inventoryItemList'
 
 export const InventoryPage = () => {
   return (
     <div>
       <InventoryHeader/>
-      InventoryPage
+      <InventoryItemList/>
     </div>
   )
 }
