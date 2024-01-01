3098326fa9e6af0f5837601a1d9ea105be11da38
yorck25 <100796096+yorck25@users.noreply.github.com>
2024-01-01 22:52:53 +0100

    * adds a main font, * adds darkmode color sheme

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HelperContextProvider } from './lib/helperContext';
+import './style.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
